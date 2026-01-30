d20cba546456e07977fdf5fd7a4270734712c278
Obura Mitchelle <oburamitchelle87@gmail.com>
2026-01-30 21:45:39 +0300

    added icon files in asset, updated app.css and about, footer section in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,72 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import myHeroImage from '/image.png'
-import myAboutImage from './assets/images/imagee.png'
+import HeroImage from './assets/images/image3.png'
+import AboutImage from './assets/images/imagee.png'
+import myLogo from './assets/images/logo.png'
+import githubLogo from './assets/images/github.svg'
+import linkedinLogo from './assets/images/linkedin.svg'
+import instagramLogo from './assets/images/instagram.svg'
+import gmailLogo from './assets/images/gmail.svg'
 import './App.css'
 
 function App() {
   return (
-    <div className="portfolio-container">
+    <body>
       {/*navigation-section*/}
-      <nav className="navbar">
-        <ul>
-          <li><a href="#about"></a>About</li>
-          <li><a href="#projects"></a>Projects</li>
-          <li><a href="#contact"></a>Contact</li>
-        </ul>
-      </nav>
+      <header className="nav-container">
+        <div className='nav-logo'>
+          <a href="#" className='mylogo'>
+            <img src={myLogo} alt="mylogo" className='mylogo' />
+          </a>
+        </div>
+        <div className='nav-bar' >
+          <button class="mobile-menu-btn">☰</button>
+          <ul className="nav-menu">
+            <li><a href="#home" className="nav-link">Home</a></li>
+            <li><a href="#about" className="nav-link">About me</a></li>
+            <li><a href="#projects" className="nav-link">Projects</a></li>
+            <li><a href="#contact" className="nav-link">Contact</a></li>
+          </ul>
+        </div>
+      </header>
       {/*hero-section*/}
-      <section className="hero">
-        <div className="hero-text">
-          <h2>Hi, I'm Mitchelle</h2>
-          <p>Aspiring Software & Web developer | Building Ideas into Code</p>
-          <button className="cta-button">View Projects</button>
-        </div>
-        <div className="hero-image">
-          <img src={myHeroImage} alt='myHeroImage'/>
-        </div>
-      </section>
-      {/*about-me-section*/}
-      <section className="about-me">
-        <div id="about">
-          <div className="container">
-            <div className="row">
-              <div className="about-col-">
-                <img src={myAboutImage} alt="myAboutImage" />
-              </div>
-              <div className="about-col-"></div>
+      <section id='home'>
+        <div className='container'>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h2>Hi, I'm Mitchelle</h2>
+              <p>Aspiring Software & Web developer | Building Ideas into Code</p>
+              <button className="cta-button">View Projects</button>
+            </div>
+            <div className="hero-image">
+              <img src={HeroImage} alt="" />
             </div>
           </div>
         </div>
-        <h1>About Me</h1>
-        <h3>I am a passionate software developer with strong interests in creating scalable, efficient and user friendly applications and websites. I enjoy turning complex problems into elegant solution and continuously improving my skills by learnnng new technologies.ith focus in creating clean, maintainable code, i aim to create a software that delivers real value and great user experience</h3>
-        <div className="tab-titles">
-          <p className="tab-links">Skills</p>
-          <p className="tab-links">Experience</p>
-          <p className="tab-links">Education</p>
-        </div>
+      </section>
+      {/*about-section*/}
+      <section>
+        <section className='about' id='about'>
+          <div className='container'>
+            <div className='about-column'>
+              <div className='about-col-1'>
+                <img src={AboutImage} alt="aboutImage" />
+              </div>
+              <div className='about-col-2'>
+                <h3>ABOUT ME</h3>
+                <p className='tablinks'></p>
+                <p className='tablinks'></p>
+                <p className='tablinks'></p>
+              </div>
+            </div>
+          </div>
+        </section>
       </section>
 
       {/*projects-section*/}
-      <section className="projects">
-        <h2>My Projects</h2>
+      <section className="projects" id='projects'>
+        <h2>Projects</h2>
         <div className="project-grid">
           <div className="project-card">
             <h3>Project one</h3>
@@ -67,14 +83,40 @@ function App() {
         </div>
       </section>
       {/*contact-section*/}
-      <section className="contact">
-        <label htmlFor="name" className="name">Name</label>
-        <input type="text" id='name' name='name' placeholder='Enter your name.' />
-        <br /><br />
-        <label htmlFor="email" className="email">Email</label>
-        <input type="email" id='email' name='' placeholder='Enter your name.' />
+      <section className="contact" id='contact'>
+        <h2>CONTACT</h2>
+        <p>I would love to here from you.Send me a message</p>
+        <form action="post">
+          <label htmlFor="Name" className="name">Name</label>
+          <input type="text" id='name' name='name' placeholder='Enter your name.' /><br /><br />
+          <label htmlFor="E mail" className="email">Email</label>
+          <input type="email" id='email' name='email' placeholder='Enter your name.' /><br /><br />
+          <label htmlFor="number" className='number'></label>
+          <input type="number" id='number' name='number' placeholder='+254700000000' /><br /><br />
+          <label htmlFor="message" className='message'>Subject</label>
+          <input type="text" id='text' name='text' placeholder='Write your message here...' /><br /><br />
+          <input type="submit" id='submit' />
+        </form>
       </section>
-    </div>
+      <footer>
+        <div className="container">
+          <p>Follow on social media for more information</p>
+          <div className='quick-links'>
+            <ul>
+              <li>
+                <a href="" className='quick-links'><img src={githubLogo} alt="github" className='logo' /></a>
+                <a href="" className='quick-links'><img src={linkedinLogo} alt="linkedin" className='logo' /></a>
+                <a href="" className='quicklinks'><img src={gmailLogo} alt="gmail" className='logo' /></a>
+                <a href="" className='quick-links'><img src={instagramLogo} alt="instagram" className='logo' /></a>
+              </li>
+            </ul>
+          </div>
+          <div class="copyright">
+            <p>&copy; 2025 michobura. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
+    </body>
   )
 }
 
